8afecbe2b1f3dd0b2a3dbff7f6f400d0227cfc42
ffaure <frederic.faure@ext.cdiscount.com>
2018-04-06 11:43:30 +0200

    printer dégueulasse...

--- src/main/java/ProblemeCavaliersPrinter.java
+++ src/main/java/ProblemeCavaliersPrinter.java
@@ -3,13 +3,20 @@ import java.util.StringJoiner;
 
 public class ProblemeCavaliersPrinter {
     public void print(List<Case> cases) {
-        for (int i = 1; i <= Echiquier.NB_LIGNES; i++) {
+        for (int i = Echiquier.NB_LIGNES; i >0; i--) {
+            System.out.print(String.format("%1d  ", i));
             StringJoiner ligneJoiner = new StringJoiner("|", "[", "]");
             for (int j = 1; j <=Echiquier.NB_COLONNES; j++) {
                 ligneJoiner.add(getPositionString(cases, i, j));
             }
             System.out.println(ligneJoiner.toString());
         }
+        StringJoiner lettresJoiner = new StringJoiner(" ", " ", " ");
+        lettresJoiner.add("  ");
+        for(char col = 'a'; col <='h'; col++) {
+            lettresJoiner.add(String.format("%1s ", col));
+        }
+        System.out.println(lettresJoiner.toString());
     }
 
     private String getPositionString(List<Case> cases, int i, int j) {
