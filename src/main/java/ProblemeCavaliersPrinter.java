1a0bc001deff78f80dac3f832695a357a0bd5de0
ffaure <frederic.faure@ext.cdiscount.com>
2018-04-09 17:45:06 +0200

    suppression des dernières boucles for dans le printer

--- src/main/java/ProblemeCavaliersPrinter.java
+++ src/main/java/ProblemeCavaliersPrinter.java
@@ -1,24 +1,36 @@
+import java.util.Collections;
 import java.util.List;
 import java.util.StringJoiner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ProblemeCavaliersPrinter {
     public void print(List<Case> cases) {
-        for (int numLigne = Echiquier.NB_LIGNES; numLigne >0; numLigne--) {
-            afficherNumeroLigne(numLigne);
-            afficherContenuLigne(cases, numLigne);
-        }
+        afficherLignes(cases);
         afficherLettresSurDerniereLigne();
     }
 
-    private void afficherNumeroLigne(int i) {
-        System.out.print(String.format("%1d  ", i));
+    private void afficherLignes(List<Case> cases) {
+        reverseIntStream(Echiquier.NB_LIGNES).forEach(numLig -> afficherLigne(cases, numLig));
+    }
+
+    private Stream<Integer> reverseIntStream(int nbLignes) {
+        return IntStream.rangeClosed(1, nbLignes).boxed().sorted(Collections.reverseOrder());
+    }
+
+    private void afficherLigne(List<Case> cases, int numLigne) {
+        afficherNumeroLigne(numLigne);
+        afficherContenuLigne(cases, numLigne);
+    }
+
+    private void afficherNumeroLigne(int numLigne) {
+        System.out.print(String.format("%1d  ", numLigne));
     }
 
     private void afficherContenuLigne(List<Case> cases, int numLigne) {
         StringJoiner ligneJoiner = new StringJoiner("|", "[", "]");
-        for (int numColonne = 1; numColonne <=Echiquier.NB_COLONNES; numColonne++) {
-            ligneJoiner.add(recupererClassementCase(cases, numLigne, numColonne));
-        }
+        IntStream.rangeClosed(1, Echiquier.NB_COLONNES)
+            .forEach(numColonne -> ligneJoiner.add(recupererClassementCase(cases, numLigne, numColonne)));
         System.out.println(ligneJoiner.toString());
     }
 
@@ -30,9 +42,9 @@ public class ProblemeCavaliersPrinter {
 
     private void afficherLettresSurDerniereLigne() {
         StringJoiner lettresJoiner = new StringJoiner(" ", "    ", " ");
-        for(char col = 'a'; col <='h'; col++) {
-            lettresJoiner.add(String.format("%1s ", col));
-        }
+        IntStream
+            .range(Case.A_NUMERIC_VALUE+1, Case.A_NUMERIC_VALUE+1+Echiquier.NB_COLONNES)
+            .forEach(col -> lettresJoiner.add(String.format("%1s ", (char)col)));
         System.out.println(lettresJoiner.toString());
     }
 
